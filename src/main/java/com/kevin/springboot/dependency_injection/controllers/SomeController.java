ef431f34b6a4901971abcf2fc0ab6bbd47e8a700
Kevin Vergel <kevinvergel@wposs.com>
2025-02-10 15:31:35 -0500

    Creando clase controlador del servicio

---
+++ src/main/java/com/kevin/springboot/dependency_injection/controllers/SomeController.java
@@ -0,0 +1,27 @@
+package com.kevin.springboot.dependency_injection.controllers;
+
+import com.kevin.springboot.dependency_injection.models.Product;
+import com.kevin.springboot.dependency_injection.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+
+    private ProductService productService = new ProductService();
+
+    @GetMapping
+    public List<Product> list() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+}
